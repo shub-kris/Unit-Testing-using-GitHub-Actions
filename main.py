2314b83dcf77069452ef66f072f95a1df501cfc6
shubham-krishna <shubhamkrishna.ism@gmail.com>
2021-05-31 13:25:09 +0200

    Initial Commit for setting up Github Actions using a simple Calculator Program

---
+++ main.py
@@ -0,0 +1,5 @@
+import calculator
+
+if __name__ == "__main__":
+    a, b = 4, 2
+    print(f"{a} + {b} = {calculator.addition(a,b)}")
---
+++ test_calculator.py
@@ -0,0 +1,6 @@
+from calculator import addition
+
+def test_run_addition():
+    result = addition(5, 2)
+    assert result == 7
+    
