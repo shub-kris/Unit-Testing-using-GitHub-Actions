f013c23dc42be781e39d4667b6c379edcd59ecf7
Shubham Krishna <shubhamkrishna.ism@gmail.com>
2021-05-31 13:41:56 +0200

    Add Subtraction feature in Calculator (#1)

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,10 @@
-from calculator import addition
+from calculator import addition, subtraction
 
 def test_run_addition():
     result = addition(5, 2)
     assert result == 7
-    
+
+def test_run_subtraction():
+    result = subtraction(5, 2)
+    assert result == 3
+
