fd73bf0b7f3c1c139a495dee5b2a0d510cbbdc9e
shubham-krishna <shubhamkrishna.ism@gmail.com>
2021-05-31 13:49:52 +0200

    Fix Division Operation

--- test_calculator.py
+++ test_calculator.py
@@ -1,16 +1,20 @@
 from calculator import addition, division, multiplication, subtraction
 
+
 def test_run_addition():
     result = addition(5, 2)
     assert result == 7
+
 
 def test_run_subtraction():
     result = subtraction(5, 2)
     assert result == 3
 
+
 def test_run_multiplication():
     result = multiplication(2, 2)
     assert result == 4
+
 
 def test_run_division():
     result = division(10, 2)
